9e5443103f4479c867d358c2b2770e231e181927
Boris Nesterko <boris.nesterko@hotmail.com>
2014-03-11 15:42:15 +0200

    Hot fix

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitTest
 {
@@ -10,6 +6,8 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
